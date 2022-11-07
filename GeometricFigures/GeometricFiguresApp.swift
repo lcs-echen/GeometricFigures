2ee7a8ba85b2f8ce50bc61bf57db0643a310be0b
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2022-11-07 12:28:02 -0500

    Initial Commit

---
+++ GeometricFigures/GeometricFiguresApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFiguresApp.swift
+//  GeometricFigures
+//
+//  Created by Evelyn Chen on 2022-11-07.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFiguresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
